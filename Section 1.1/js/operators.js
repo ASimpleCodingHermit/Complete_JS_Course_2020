b9f4e22bd0347d49a5c1c65d481055053c5bc8e4
Joseph Chu <jchucodes@gmail.com>
2021-01-01 22:26:34 -0500

    Updated for Logical Operators

--- Section 1.1/js/operators.js
+++ Section 1.1/js/operators.js
@@ -1,5 +1,32 @@
 var date = new Date();
 var currentYear = date.getFullYear();
 console.log(currentYear);
-var result, nextYear, intoFuture;
+var result, nextYear, intoFuture, dob, three;
 dob = 1984;
+// math operator minus
+result = currentYear - dob;
+console.log(result);
+
+// math operator plus
+nextYear = 1;
+result = currentYear + nextYear;
+console.log(result);
+
+// math operator multiply
+three = 3;
+result = currentYear * three;
+console.log(result);
+
+// math operator division
+three = 3;
+result = currentYear / three;
+console.log(result);
+
+// LOGICAL OPERATORS >, <
+var first, second, isBigger, isSmaller;
+first = 10;
+second = 5;
+isBigger = first > second;
+isBigger = first < second;
+console.log("isBigger: " + isBigger);
+console.log("isSmaller: " + isSmaller);
